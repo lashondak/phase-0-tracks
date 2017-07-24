5b7f624a29f230ff0a618828db0d6bb85743983f
LaShonda <lashonda5@LDK-MacBook-Air.local>
2017-07-24 11:55:04 -0500

    Add code to collect user information and preferences.

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,22 +1,34 @@
-puts “What is your name?”
-	employee_name = gets.chomp
+puts "Good day! Welcome to Vampire Enterprises of America!"
+puts
 
-puts “How old are you?”
-	employee_age = gets.chomp
+puts "What is your name?"
+	  employee_name = gets.chomp
+puts
+	
+puts "Hello #{employee_name}! How many new employees will we be processing today?" 
+    number_of_employees = gets.chomp.to_i 
+puts
 
-puts “What year were you born?”
-	employee_birth_year = gets.chomp
+#create an while loop that stops when number_of_employees == to input and increments it by 1 after every attempt.
 
-puts “Our cafeteria has AMAZING garlic bread! Would you like us to order some for you?”
-	garlic_bread_preference = gets.chomp
+puts "How old are you?"
+    employee_age = gets.chomp.to_i
+puts
 
-puts “We have an extensive health insurance options. Would you like to enroll in the plan?”
-	health_insurance_preference = gets.chomp
+puts "What year were you born in?"
+	  employee_birth_year = gets.chomp.to_i
+puts
 
-puts employee_name
-puts employee_age
-puts employee_birth_year
-puts garlic_bread_preference
-puts health_insurance_preference
+puts "Our cafeteria has AMAZING garlic bread! Would you like us to order some for you?"
+  garlic_bread_preference = gets.chomp
+puts
 
+puts "We have amazing health insurance options. Would you like to enroll in the plan?"
+    health_insurance_preference = gets.chomp
+puts
 
+#puts employee_name
+#puts employee_age
+#puts employee_birth_year
+#puts garlic_bread_preference
+#puts health_insurance_preference
