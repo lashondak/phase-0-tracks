c972c10d59a3a9a542833605e5a968f21087f09a
LaShonda <lashonda5@LDK-MacBook-Air.local>
2017-07-23 22:35:51 -0500

    Add initial variable declarations to Vampire Detection Program.

---
+++ ruby/vampires.rb
@@ -0,0 +1,22 @@
+puts “What is your name?”
+	employee_name = gets.chomp
+
+puts “How old are you?”
+	employee_age = gets.chomp
+
+puts “What year were you born?”
+	employee_birth_year = gets.chomp
+
+puts “Our cafeteria has AMAZING garlic bread! Would you like us to order some for you?”
+	garlic_bread_preference = gets.chomp
+
+puts “We have an extensive health insurance options. Would you like to enroll in the plan?”
+	health_insurance_preference = gets.chomp
+
+puts employee_name
+puts employee_age
+puts employee_birth_year
+puts garlic_bread_preference
+puts health_insurance_preference
+
+
