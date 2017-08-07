e5fb32217f6deccd043abde4220f7d0a54f6be3a
LaShonda <lashonda5@LDK-MacBook-Air.local>
2017-08-06 21:30:51 -0700

    Created a interior design application using hashes.

---
+++ ruby/interior_design.rb
@@ -0,0 +1,19 @@
+interior_design = {
+
+name: = "Ms. Fancy" 
+address: = "123 N. Baller Dr., Hollywood, CA 90210"
+email: = "msfancy1@gmail.com"
+phone: = "123-555-1212"
+blue_shade: = "Periwinkle"
+wallpaper_pref: = "Paisley"
+ombre: = "Fierce"
+
+}
+
+p post[name]
+
+post[hired] = "Yes" 
+
+
+
+#
